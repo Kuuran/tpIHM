7e6a5bf310639c7923bc70b2118b89e50f97c287
Kuuran <felix.jacquemin@u-psud.fr>
2018-06-08 12:28:28 +0200

    ça marche un peu mais maintenant faut dessiner un rectangle

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,15 +1,33 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Rectangle;
 
 public class Controller {
 
     private Mode mode;
+    private final ToggleGroup tg = new ToggleGroup();
 
-    public Controller(){
+    @FXML private javafx.scene.control.Button cloneB;
+    @FXML private javafx.scene.control.Button deleteB;
+
+    @FXML private javafx.scene.control.RadioButton select;
+    @FXML private javafx.scene.control.RadioButton ellipse;
+    @FXML private javafx.scene.control.RadioButton rectangle;
+    @FXML private javafx.scene.control.RadioButton line;
+
+    @FXML public void initialize(){
         mode = null;
+
+        select.setToggleGroup(tg);
+        ellipse.setToggleGroup(tg);
+        rectangle.setToggleGroup(tg);
+        line.setToggleGroup(tg);
     }
+
 
     public Mode getMode() {
         return mode;
@@ -30,23 +48,37 @@ public class Controller {
 
     public void SelectRadio(ActionEvent actionEvent) {
         mode = Mode.SELECT;
+        cloneB.setDisable(false);
+        deleteB.setDisable(false);
+
+
     }
 
     public void EllipseRadio(ActionEvent actionEvent) {
         mode = Mode.ELLIPSE;
+        cloneB.setDisable(true);
+        deleteB.setDisable(true);
     }
 
     public void RectangleRadio(ActionEvent actionEvent) {
         mode = Mode.RECTANGLE;
+        cloneB.setDisable(true);
+        deleteB.setDisable(true);
     }
 
     public void LineRadio(ActionEvent actionEvent) {
         mode = Mode.LINE;
+        cloneB.setDisable(true);
+        deleteB.setDisable(true);
     }
 
     public void CLickPane(MouseEvent mouseEvent) {
+        double pos_x = mouseEvent.getX();
+        double pos_y = mouseEvent.getY();
+
         switch (mode){
             case RECTANGLE:
+                Rectangle rectangle = new Rectangle(pos_x, pos_y, 30, 15);
                 break;
             case ELLIPSE:
                 break;
