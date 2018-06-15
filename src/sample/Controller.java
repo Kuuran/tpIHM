869306b63926028e480d536f6d916f93b368d9a5
Felix <felix.jacquemin@etu.univ-lyon1.fr>
2018-06-15 23:36:17 +0200

    drag des objets

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,15 +1,19 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Ellipse;
+import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 
 public class Controller {
 
     private Mode mode;
     private final ToggleGroup tg = new ToggleGroup();
+    private double posx, posy, transx, transy;
 
     @FXML private javafx.scene.control.Button cloneB;
     @FXML private javafx.scene.control.Button deleteB;
@@ -18,6 +22,8 @@ public class Controller {
     @FXML private javafx.scene.control.RadioButton ellipse;
     @FXML private javafx.scene.control.RadioButton rectangle;
     @FXML private javafx.scene.control.RadioButton line;
+    @FXML private javafx.scene.layout.Pane pane;
+    @FXML private javafx.scene.control.ColorPicker color;
 
     @FXML public void initialize(){
         mode = null;
@@ -78,11 +84,25 @@ public class Controller {
 
         switch (mode){
             case RECTANGLE:
-                Rectangle rectangle = new Rectangle(pos_x, pos_y, 30, 15); //todo
+                Rectangle rectangle = new Rectangle(pos_x, pos_y, 30, 15);
+                rectangle.setFill(color.getValue());
+                rectangle.setOnMousePressed(RectPressed);
+                rectangle.setOnMouseDragged(RectDragged);
+                pane.getChildren().add(rectangle);
                 break;
             case ELLIPSE:
+                Ellipse ellipse = new Ellipse(pos_x, pos_y, 20, 30);
+                ellipse.setFill(color.getValue());
+                ellipse.setOnMousePressed(EllipsePressed);
+                ellipse.setOnMouseDragged(EllipseDragged);
+                pane.getChildren().add(ellipse);
                 break;
             case LINE:
+                Line line = new Line(pos_x, pos_y, 30, 15);
+                line.setFill(color.getValue());
+                line.setOnMousePressed(LinePressed);
+                line.setOnMouseDragged(LineDragged);
+                pane.getChildren().add(line);
                 break;
             case SELECT:
                 break;
@@ -91,6 +111,92 @@ public class Controller {
         }
     }
 
-    public void DragPane(MouseEvent mouseEvent) {
-    }
+
+    EventHandler<MouseEvent> RectPressed =
+            new EventHandler<MouseEvent>() {
+                @Override
+                public void handle (MouseEvent t){
+                    if(mode == Mode.SELECT) {
+                        posx = t.getSceneX();
+                        posy = t.getSceneY();
+                        transx = ((Rectangle) (t.getSource())).getTranslateX();
+                        transy = ((Rectangle) (t.getSource())).getTranslateY();
+                    }
+                }
+            };
+    EventHandler<MouseEvent> RectDragged =
+            new EventHandler<MouseEvent>() {
+
+                @Override
+                public void handle(MouseEvent t) {
+                    if(mode == Mode.SELECT) {
+                        double offsetX = t.getSceneX() - posx;
+                        double offsetY = t.getSceneY() - posy;
+                        double newTranslateX = transx + offsetX;
+                        double newTranslateY = transy + offsetY;
+
+                        ((Rectangle) (t.getSource())).setTranslateX(newTranslateX);
+                        ((Rectangle) (t.getSource())).setTranslateY(newTranslateY);
+                    }
+                }
+            };
+
+    EventHandler<MouseEvent> LinePressed =
+            new EventHandler<MouseEvent>() {
+                @Override
+                public void handle (MouseEvent t){
+                    if(mode == Mode.SELECT) {
+                        posx = t.getSceneX();
+                        posy = t.getSceneY();
+                        transx = ((Line) (t.getSource())).getTranslateX();
+                        transy = ((Line) (t.getSource())).getTranslateY();
+                    }
+                }
+            };
+    EventHandler<MouseEvent> LineDragged =
+            new EventHandler<MouseEvent>() {
+
+                @Override
+                public void handle(MouseEvent t) {
+                    if(mode == Mode.SELECT) {
+                        double offsetX = t.getSceneX() - posx;
+                        double offsetY = t.getSceneY() - posy;
+                        double newTranslateX = transx + offsetX;
+                        double newTranslateY = transy + offsetY;
+
+                        ((Line) (t.getSource())).setTranslateX(newTranslateX);
+                        ((Line) (t.getSource())).setTranslateY(newTranslateY);
+                    }
+                }
+            };
+
+    EventHandler<MouseEvent> EllipsePressed =
+            new EventHandler<MouseEvent>() {
+                @Override
+                public void handle (MouseEvent t){
+                    if(mode == Mode.SELECT) {
+                        posx = t.getSceneX();
+                        posy = t.getSceneY();
+                        transx = ((Ellipse) (t.getSource())).getTranslateX();
+                        transy = ((Ellipse) (t.getSource())).getTranslateY();
+                    }
+                }
+            };
+    EventHandler<MouseEvent> EllipseDragged =
+            new EventHandler<MouseEvent>() {
+
+                @Override
+                public void handle(MouseEvent t) {
+                    if(mode == Mode.SELECT) {
+                        double offsetX = t.getSceneX() - posx;
+                        double offsetY = t.getSceneY() - posy;
+                        double newTranslateX = transx + offsetX;
+                        double newTranslateY = transy + offsetY;
+
+                        ((Ellipse) (t.getSource())).setTranslateX(newTranslateX);
+                        ((Ellipse) (t.getSource())).setTranslateY(newTranslateY);
+                    }
+                }
+            };
 }
+
